9dddf25ad34686b91be250a36dabaaee7cb1ea47
wipegup <wipegup@gmail.com>
2019-02-20 09:15:30 -0700

    Add test for it exists market

--- test/market_test.rb
+++ test/market_test.rb
@@ -4,5 +4,12 @@ require './lib/market'
 require './lib/vendor'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+  
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
 
 end
