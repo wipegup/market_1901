a249b024661455e8d2163168571afc9d0e02f5d7
wipegup <wipegup@gmail.com>
2019-02-20 09:16:55 -0700

    Add test for name; market

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,9 +7,13 @@ class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
   end
-  
+
   def test_it_exists
     assert_instance_of Market, @market
+  end
+
+  def test_it_has_a_name
+    assert_equal "South Pearl Street Farmers Market", @market.name
   end
 
 end
